98c1b25624fa17c47d5552ff811db0882cf49bc0
KonradSmuga <32478813+KonradSmuga@users.noreply.github.com>
2018-01-31 14:21:30 +0100

    Update FirstChallenge.java

--- Exception/src/main/java/com/kodilla/exception/test/FirstChallenge.java
+++ Exception/src/main/java/com/kodilla/exception/test/FirstChallenge.java
@@ -1,20 +1,13 @@
 package com.kodilla.exception.test;
 
-
 public class FirstChallenge {
+
 
     public double divide(double a, double b) throws ArithmeticException {
         if (b == 0) {
-
-        }
-        try {
-            return a / b;
-
-        } catch (ArithmeticException e) {
             throw new ArithmeticException();
-        } finally {
-            System.out.println("try to not divide by 0!");
         }
+        return a / b;
     }
 
     /**
@@ -25,14 +18,14 @@ public class FirstChallenge {
     public static void main(String[] args) {
 
         FirstChallenge firstChallenge = new FirstChallenge();
-
+        try {
         double result = firstChallenge.divide(3, 0);
+            System.out.println(result);
 
-        System.out.println(result);
-
+        } catch (ArithmeticException e) {
+            System.out.println("Dzielenie nie powiodlo się " +e);
+        } finally {
+            System.out.println("Nie dzielimy przez 0 ");
+        }
     }
 }
-
-/*
- }
- */
