e91d82eb155f7cca92d0dfb9141387e09e1f2161
KonradSmuga <smuggk@gmail.com>
2018-03-19 16:47:53 +0100

    Clean Code

---
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/Allegro/User.java
@@ -0,0 +1,51 @@
+package com.kodilla.good.patterns.challenges.Allegro;
+import java.util.Date;
+
+public class User {
+    private final String name;
+    private final String username;
+    private final String email;
+    private final Date dateOfBirth;
+    private final String nickName;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public Date getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public String getNickName() {
+        return nickName;
+    }
+
+    public User(String name, String username, String email, Date dateOfBirth, String nickName) {
+        this.name = name;
+        this.username = username;
+        this.email = email;
+        this.dateOfBirth = dateOfBirth;
+        this.nickName = nickName;
+
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", username='" + username + '\'' +
+                ", email='" + email + '\'' +
+                ", dateOfBirth=" + dateOfBirth +
+                ", nickName='" + nickName + '\'' +
+                '}';
+    }
+}
