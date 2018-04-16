08f9ba8288dee4681949475d6757e99b2cf6a857
KonradSmuga <smuggk@gmail.com>
2018-04-16 16:57:18 +0200

    20.1 Facade pattern

---
+++ kodilla-patterns2/src/main/java/com/kodilla/patterns2/KodilaPatterns2Application.java
@@ -0,0 +1,12 @@
+package com.kodila.patterns2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KodilaPatterns2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KodilaPatterns2Application.class, args);
+	}
+}
---
+++ kodilla-patterns2/src/main/java/com/kodilla/patterns2/facade/Authentication.java
@@ -0,0 +1,15 @@
+package com.kodilla.patterns2.facade;
+
+import org.springframework.stereotype.Service;
+
+import java.util.Random;
+
+@Service
+public class Authentication {
+    public boolean isAuthenticated(Long userId) {
+        Random random = new Random();
+        return random.nextBoolean();
+    }
+
+
+}
